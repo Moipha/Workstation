07bb969bc8930f146a57a2744550df530d7c2e8e
Moipha <chengfayang@henu.edu.cn>
2025-12-09 23:23:19 +0800

    init

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<template>
+    <NuxtLayout>
+        <header class="font-bold text-6xl">我来组成头部</header>
+        <main>
+            <NuxtPage></NuxtPage>
+        </main>
+    </NuxtLayout>
+</template>
+<script setup lang="ts"></script>
---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="min-h-screen bg-gray-100 p-8">
+        <h1 class="text-4xl font-bold text-blue-600">哈哈哈哈</h1>
+        <div class="mt-4">
+            <NuxtPage></NuxtPage>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
